544ffa6281babf6d622ef6a37847bcbe29d8511d
Damir Aliyev <daliev.com@gmail.com>
2022-07-17 23:55:54 +0300

    added server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/update/{type}/{key}/{value}", handler).Methods("POST")
+
+	if err := http.ListenAndServe("localhost:8080", router); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	fmt.Fprintf(w, "%v %v %v\n", vars["type"], vars["key"], vars["value"])
+}
